9dbf121591a41f694c0fbfd13cc44cae16906768
idtechie <78948468+idtechie@users.noreply.github.com>
2023-09-17 16:25:47 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "SOT Project"
+copyright = "2023, SOT team"
+author = "SOT team"
 
 
 # -- General configuration ---------------------------------------------------
